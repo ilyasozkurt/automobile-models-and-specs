f3f288adf4c8f2292749c1385ce65de75d594d0f
ilyasozkurt <ilyas.ozkurt@beyn.com.tr>
2021-12-08 11:33:37 +0300

    automobiles() relation added.

--- scrapper/app/Models/Brand.php
+++ scrapper/app/Models/Brand.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Brand extends Model
@@ -24,4 +25,12 @@ class Brand extends Model
         'name',
         'logo',
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function automobiles(): HasMany
+    {
+        return $this->hasMany('App\Models\Automobile');
+    }
 }
