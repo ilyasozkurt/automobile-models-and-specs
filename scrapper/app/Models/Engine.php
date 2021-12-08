72f76692e6a54f4ebd7f50c4ef1a0707b2dc5e3a
ilyasozkurt <ilyas.ozkurt@beyn.com.tr>
2021-12-08 11:33:22 +0300

    Engine model created.

---
+++ scrapper/app/Models/Engine.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Engine extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'engines';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'hash',
+        'automobile_id',
+        'name',
+        'specs'
+    ];
+
+    protected $casts = [
+        'specs' => 'array'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function engines(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\Automobile');
+    }
+
+}
